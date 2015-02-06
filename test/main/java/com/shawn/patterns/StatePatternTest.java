6599611479197cafb9c347359eeb7f415434eb79
biubiu <c.shangfei@gmail.com>
2015-02-06 12:08:41 +0800

    add "state pattern"

---
+++ test/main/java/com/shawn/patterns/StatePatternTest.java
@@ -0,0 +1,54 @@
+package com.shawn.patterns;
+
+import com.state.demo.GumballMachine;
+import org.junit.Test;
+
+/**
+ * @author Shawn Cao
+ */
+public class StatePatternTest {
+
+    @Test
+    public void testGumballMachineState(){
+        GumballMachine gumballMachine =
+                new GumballMachine(10);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+
+}
